db6adcc6eb4b30f54e7d8b701e0cdcf9de43e393
zayaserg <zayaserg@gmail.com>
2020-02-08 12:12:29 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import "./plugins/vue-tailwind";
+import "./assets/css/tailwind.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import Transitions from "vue2-transitions";
+Vue.use(Transitions);
+
+import MyOwnTheme from "./myOwnTheme.js";
+import VueTailwind from "vue-tailwind";
+Vue.use(VueTailwind, {
+  theme: MyOwnTheme
+});
 
 Vue.config.productionTip = false;
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+import FooterLogin from "./layout/FooterLogin";
+import Landing from "./views/Landing.vue";
+import Solar from "./views/Solar.vue";
+import Components from "./views/Components.vue";
+import Test from "./views/Test.vue";
+import VueTailwind from "./views/VueTailwind.vue";
+import Login from "./views/Login.vue";
+import Login2 from "./views/Login2.vue";
+import Profile from "./views/Profile.vue";
+import Dashboard from "./views/Dashboard.vue";
+// import Swiper from "./views/Swiper.vue";
 
 Vue.use(Router);
 
@@ -10,8 +22,84 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "landing",
+      components: {
+        header: Navbar,
+        default: Landing,
+        footer: Footer
+      }
+    },
+    {
+      path: "/solar",
+      name: "solar",
+      components: {
+        // header: Navbar,
+        default: Solar
+        // footer: Footer
+      }
+    },
+    {
+      path: "/components",
+      name: "components",
+      components: {
+        header: Navbar,
+        default: Components
+        // footer: Footer
+      }
+    },
+    {
+      path: "/test",
+      name: "test",
+      components: {
+        // header: Navbar,
+        default: Test
+        // footer: Footer
+      }
+    },
+    {
+      path: "/vue-tailwind",
+      name: "vue-tailwind",
+      components: {
+        // header: Navbar,
+        default: VueTailwind
+        // footer: Footer
+      }
+    },
+    {
+      path: "/login",
+      name: "login",
+      components: {
+        header: Navbar,
+        default: Login,
+        footer: FooterLogin
+      }
+    },
+    {
+      path: "/login2",
+      name: "login2",
+      components: {
+        header: Navbar,
+        default: Login2,
+        footer: FooterLogin
+      }
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      components: {
+        header: Navbar,
+        default: Profile,
+        footer: Footer
+      }
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      components: {
+        // header: Navbar,
+        default: Dashboard
+        // footer: Footer
+      }
     },
     {
       path: "/about",
@@ -21,6 +109,24 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/swiper",
+      name: "swiper",
+      component: () =>
+        import(/* webpackChunkName: "components" */ "./views/Swiper.vue")
+    },
+    {
+      path: "/button",
+      name: "button",
+      component: () =>
+        import(/* webpackChunkName: "components" */ "./views/Button.vue")
     }
+    // {
+    //   path: "/landing",
+    //   name: "landing",
+    //   component: () =>
+    //     import(/* webpackChunkName: "components" */ "./views/Landing.vue")
+    // }
   ]
 });
